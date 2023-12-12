0f6ec8418d9bed3cac974fa31f096a4966ac9728
Nikolai Markalainen <nikolai.markalainen@gmail.com>
2023-12-12 19:26:18 +0200

    Initial version

---
+++ backend/api.py
@@ -0,0 +1,105 @@
+
+UPLOAD_FOLDER = 'uploads'
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+import os
+
+
+from flask import render_template
+from flask_cors import CORS
+from flask import Flask, flash, request, redirect, url_for, jsonify
+from werkzeug.utils import secure_filename
+
+import numpy as np
+import tensorflow as tf
+import base64
+
+from tensorflow.keras.preprocessing import image
+from keras.models import load_model
+
+
+app = Flask(__name__)
+CORS(app)
+
+#verifying data 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+#loading ai model
+def load_model_from_file():
+
+    myModel = load_model('saved_model.h5')
+    return (myModel)
+
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        print("SHOULD BE HERE", request)
+        # Request has files in it
+        if 'file' not in request.json:
+            print(request.json['file'])
+            return jsonify({'message': 'Error: No file part'})
+        file = request.json['file']
+        # If filename is empty
+        if 'fileName' not in request.json:
+            return jsonify({'message': 'Error: No selected file'})
+        filename = request.json['fileName']
+        if not allowed_file(filename):
+            return jsonify({'message': 'Error: Wrong file format'})
+        if file and allowed_file(filename):
+            print("should be here as everything matches")
+            filename = secure_filename(filename)
+            img_data = base64.b64decode(file)
+            img_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            with open(img_path, 'wb') as f:
+                f.write(img_data)
+            result = uploaded_file(filename)
+        if result < 0.5:
+            return jsonify({'message': 'Uploaded image is a rubiks cube'})
+        else:
+            return jsonify({'message': 'Not a cube'})
+        
+def uploaded_file(filename):
+    test_image = image.load_img(UPLOAD_FOLDER + "/" + filename, target_size=(128, 128))
+    test_image = image.img_to_array(test_image)
+    test_image = test_image / 255.0
+    test_image = np.expand_dims(test_image, axis=0)
+
+    myModel = app.config['MODEL']
+    result = myModel.predict(test_image)
+    image_src = "./" + UPLOAD_FOLDER + "/" + filename
+
+
+    if(result < 0.5):
+        print("WE HAVE A RESULT")
+        response_data = {
+        'result': result,
+        'image_src': image_src,
+        }
+        return result
+    else:
+        print("WHAT IS THIS")
+        response_data = {
+        'result': result,
+        'image_src': image_src,
+        }
+        return result
+
+def main():
+    (myModel) = load_model_from_file()
+    
+    app.config['SECRET_KEY'] = 'super_secret_key'
+    app.config['MODEL'] = myModel
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+    app.run(port=5001)
+
+results = []
+
+
+
+
+main()
---
+++ backend/trainingmodel.py
@@ -0,0 +1,38 @@
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import os
+
+input_directory = "./train/y"
+output_directory = "./previewY"
+
+# Create an ImageDataGenerator
+datagen = ImageDataGenerator(
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    rescale=1.0 / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode="nearest"
+)
+
+file_list = os.listdir(input_directory)
+
+for filename in file_list:
+    if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):
+        # Load the image
+        img = load_img(os.path.join(input_directory, filename))
+
+        x = img_to_array(img)
+        x = x.reshape((1, ) + x.shape)
+
+        i = 0
+        for batch in datagen.flow(x,
+                                  batch_size=1,
+                                  save_to_dir=output_directory,
+                                  save_prefix=os.path.splitext(filename)[0],
+                                  save_format='jpeg'):
+            i += 1
+            if i > 20:
+                break
